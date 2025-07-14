d43015691e91aedb782070d5f35952333bd4ab1f
BaoNG <9Bao_Nguyengia@singaporeair.com.sg>
2025-07-14 14:09:13 +0700

    build: update docker-compose.yml file

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -21,8 +21,8 @@ const corsOptions = {
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
-    if (process.env.MONGO_URL) {
-      mongoose.connect(process.env.MONGO_URL, {
+    if (process.env.MONGO_URI) {
+      mongoose.connect(process.env.MONGO_URI, {
         //   useNewUrlParser: true,
         //   useUnifiedTopology: true,
       });
